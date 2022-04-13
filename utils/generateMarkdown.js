883f3663de6d0cfe51eec1471bb2fc36f0e1bfc8
Your Name <98237529+Dmj22@users.noreply.github.com>
2022-04-13 19:26:05 -0400

    added license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {console.log(license)
+    let licenseType = license;
+    let altLicense = ''
+    
+    if(licenseType === 'MIT') {
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    } else if (licenseType === 'Boost') {
+      "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+    } else if (licenseType === 'Mozilla') { "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+      
+    } else {
+      licenseType = altLicense
+    }
+    return licenseType;
+  
+  };
+  
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
