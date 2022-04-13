bea03be1713218570be8db5fa764579d3ae97e18
Your Name <98237529+Dmj22@users.noreply.github.com>
2022-04-13 19:27:14 -0400

    added license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,30 @@ function renderLicenseBadge(license) {console.log(license)
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    console.log(license)
+  let licenseLink = license;
+  let altLicense = ''
+  
+  if(license === 'MIT') { 
+    licenseLink= "[https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)"
+  
+  } else if (license === 'Boost') { 
+    licenseLink= "[https://opensource.org/licenses/BSL-1.0](https://opensource.org/licenses/BSL-1.0)"
+  
+  } else if (licenseLink === 'Mozilla') { 
+    licenseLink= "[https://opensource.org/licenses/MPL-2.0](https://opensource.org/licenses/MPL-2.0)"
+    
+  } else {
+    licenseLink = altLicense
+  }
+  return licenseLink;
+
+
+
+
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
